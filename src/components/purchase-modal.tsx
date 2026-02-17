d14bcae8161f0f48b97f0061755133733432900c
sayan2nd <sm1555524@egmail.com>
2026-02-17 16:32:26 +0000

    chnage the upi id to "garenaff-store-mondal@yesg"

--- src/components/purchase-modal.tsx
+++ src/components/purchase-modal.tsx
@@ -371,7 +371,7 @@ export default function PurchaseModal({ product, user: initialUser, onClose }: P
             </>
         );
     case 'qrPayment':
-        const upiId = "garena-store-sayan@yesg";
+        const upiId = "garenaff-store-mondal@yesg";
         const upiUrl = `upi://pay?pa=${upiId}&pn=Garena&am=${finalPrice}&cu=INR&tn=${product.name}`;
         const minutes = Math.floor(qrCountdown / 60);
         const seconds = qrCountdown % 60;
@@ -519,4 +519,5 @@ export default function PurchaseModal({ product, user: initialUser, onClose }: P
 }
     
 
+    
     
